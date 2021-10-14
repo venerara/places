85617509cf37e38d6127f0294fea2ec6a0bd22ba
Venerara <ufa.venera@gmail.com>
2021-10-14 16:48:28 +0500

    update1 main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,60 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyFirstWidget(),
+    );
+  }
+}
+
+class MyFirstWidget extends StatelessWidget {
+  const MyFirstWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print('build');
+    return Container(
+      child: Center(
+        child: Text('Hello!'),
+      ),
+    );
+  }
+}
+
+class MyFirstfulWidget extends StatefulWidget {
+  const MyFirstfulWidget({Key? key}) : super(key: key);
+
+  @override
+  _MyFirstfulWidgetState createState() => _MyFirstfulWidgetState();
+}
+
+class _MyFirstfulWidgetState extends State<MyFirstfulWidget> {
+  int _ccounter = 0;
+  ret() {
+    return (context.runtimeType);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _ccounter++;
+    print('$_ccounter');
+    return Container(
+      child: Center(
+        child: Text('Hello!'),
+      ),
     );
   }
 }
