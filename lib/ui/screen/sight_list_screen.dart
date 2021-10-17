71dc61e55bb121c1294c027c87c1dd1090c0af72
Venerara <ufa.venera@gmail.com>
2021-10-17 14:50:19 +0500

    update2 main.dart

---
+++ lib/ui/screen/sight_list_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SightListScreen extends StatefulWidget {
+  const SightListScreen({Key? key}) : super(key: key);
+
+  @override
+  _SightListScreenState createState() => _SightListScreenState();
+}
+
+class _SightListScreenState extends State<SightListScreen> {
+  int count = 0;
+  void incrementcount() {
+    setState(() {
+      count++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Список интересных мест'),
+      ),
+    );
+  }
+}
