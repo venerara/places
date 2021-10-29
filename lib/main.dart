b5e3388a734430b47eaa0cbed33d5f2c549a743d
Venerara <ufa.venera@gmail.com>
2021-10-30 02:07:55 +0500

    Create sight card

--- lib/main.dart
+++ lib/main.dart
@@ -1,128 +1,22 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:places/ui/screen/Sight_list_screen.dart';
+import 'package:places/ui/screen/sight_card.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: 'Flutter Demo App',
+      theme: ThemeData(),
       home: const SightListScreen(),
-    );
-  }
-}
-
-class SightListScreen extends StatefulWidget {
-  const SightListScreen({Key? key}) : super(key: key);
-
-  @override
-  _SightListScreenState createState() => _SightListScreenState();
-}
-
-class _SightListScreenState extends State<SightListScreen> {
-  int count = 0;
-  void incrementcount() {
-    setState(() {
-      count++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // Изменение высоты Appbar
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(80),
-        child: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0, // Убираем тень
-          // Создание заголовка в 2 строки с разноцветными буквами
-          title: RichText(
-            text: TextSpan(
-                text: "С",
-                style: TextStyle(
-                    color: Colors.green,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 26),
-                children: [
-                  TextSpan(
-                    text: "писок\n",
-                    style: TextStyle(
-                      color: Colors.black,
-                    ),
-                  ),
-                  TextSpan(
-                      text: "и",
-                      style: TextStyle(
-                        color: Colors.yellow,
-                      ),
-                      children: [
-                        TextSpan(
-                          text: "нтересных мест",
-                          style: TextStyle(
-                            color: Colors.black,
-                          ),
-                        ),
-                      ]),
-                ]),
-            maxLines: 2,
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
